c0c1f791d57d0fb51a19a7b90f9c6edaf3f1cb28
gwdowner <gwdowner@outlook.com>
2020-04-01 13:24:12 +0100

    Add build pipeline

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM node:12.16.1
+COPY . /app
+WORKDIR /app
+ENV PORT=8080
+EXPOSE 8080
+CMD ['npm', 'start']
---
+++ server/test/test.js
@@ -0,0 +1,7 @@
+const assert = require('chai').assert;
+
+describe('placeholder test', ()=>{
+    it('random test',()=>{
+        assert.isTrue(true);
+    })
+});
